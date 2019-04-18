6d59eef6c361440a866a78484289fba2919fad65
yokernorris <davidnorris666@gmail.com>
2019-04-18 19:22:57 +0200

    update

---
+++ 01-holaMundo/src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+//Componente que se va a encargar del header
+import {Component} from '@angular/core';
+
+//Decorador para el componente
+//de boostrap
+@Component({
+  selector: 'app-header',
+  //template `` para incluir tu el codigo
+  //remplateUrl '' para importar html
+  templateUrl: './header.component.html'
+})
+
+export class HeaderComponent{
+
+}
