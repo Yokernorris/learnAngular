43fc9afae623f2b4786ee8d2454f22a179ee753d
yokernorris <davidnorris666@gmail.com>
2019-04-18 19:56:05 +0200

    update

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NgModule } from '@angular/core';
 import {APP_ROUTING} from './app.routes';
 
 //Servicios
+import {HeroesService} from './services/heroes.service';
 
 
 
@@ -28,7 +29,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- spa/src/app/components/heroes/heroes.component.ts
+++ spa/src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroesService, Heroe} from '../../services/heroes.service'
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
+  heroes:Heroe[] = [];
+  constructor(private _heroesService:HeroesService) {
 
-  constructor() { }
+
+  }
 
   ngOnInit() {
+
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
+
+
